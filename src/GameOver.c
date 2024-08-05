b4df1c7fcdc409ed682c3ea445e4b8cd5aabe51c
Anthony Lesch <23031998+GutterBeats@users.noreply.github.com>
2024-08-05 08:49:48 -0700

    Finished initial game over screen.

--- src/GameOver.c
+++ src/GameOver.c
@@ -4,32 +4,77 @@
 
 #include <stdbool.h>
 
+#include "Audio.h"
+#include "Game.h"
+#include "Renderer.h"
+#include "Resources.h"
+#include "Text.h"
+#include "Utils.h"
+
+#define TEXT_PADDING 15
+
+//----------------------------------------------------------------------------------
+// Game Over Screen Variables
+//----------------------------------------------------------------------------------
+static Texture* background;
+static Texture* gameOver;
+static Texture* message;
+static Texture* restart;
+static bool shouldFinish;
+static Vector2D gameOverTextPosition;
+static Vector2D messageTextPosition;
+static Vector2D restartMessagePosition;
+
+//----------------------------------------------------------------------------------
+// Game Over Screen Functions
+//----------------------------------------------------------------------------------
 void InitGameOverScreen(void)
 {
+    background = REN_LoadTexture(BACKGROUND_IMAGE);
+    shouldFinish = false;
 
+    int windowWidth, windowHeight;
+    GAM_GetScreenDimensions(&windowWidth, &windowHeight);
+
+    const char* text = GAM_GetGameWon() ? "You Won!" : "You Lost!";
+
+    gameOver = TXT_CreateTextureFromText_Large("GAME OVER");
+    message = TXT_CreateTextureFromText(text);
+    restart = TXT_CreateTextureFromText("Press the spacebar to play again!");
+
+    gameOverTextPosition = UTL_MakeVector2D(windowWidth / 2.f - gameOver->Width / 2.f, windowHeight / 5.f);
+    messageTextPosition = UTL_MakeVector2D(windowWidth / 2.f - message->Width / 2.f, gameOverTextPosition.Y + gameOver->Height + TEXT_PADDING);
+    restartMessagePosition = UTL_MakeVector2D(windowWidth / 2.f - restart->Width / 2.f, messageTextPosition.Y + message->Height + TEXT_PADDING);
+
+    AUD_PlayMusic(GAMEOVER_MUSIC);
 }
 
-void UpdateGameOverScreen(float deltaTime)
+void UpdateGameOverScreen(const float deltaTime)
 {
-
 }
 
 void DrawGameOverScreen(void)
 {
-
+    REN_DrawTexture_Alpha(background, UTL_GetZeroVector(), BB_BACKGROUND_ALPHA);
+    REN_DrawTexture(gameOver, gameOverTextPosition);
+    REN_DrawTexture(message, messageTextPosition);
+    REN_DrawTexture(restart, restartMessagePosition);
 }
 
 void UnloadGameOverScreen(void)
 {
-
+    REN_FreeTexture(background);
+    REN_FreeTexture(gameOver);
+    REN_FreeTexture(message);
+    REN_FreeTexture(restart);
 }
 
 bool FinishGameOverScreen(void)
 {
-
+    return shouldFinish;
 }
 
 void GameOverEnterKeyPressed(void)
 {
-
+    shouldFinish = true;
 }
